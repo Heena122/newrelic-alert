dc8376b58430ab1784475b7b6ed0d41e19c0366d
Heena122 <126157263+Heena122@users.noreply.github.com>
2024-05-16 13:39:16 +0530

    first commit

---
+++ modules/dashboard/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  # Require Terraform version 1.0 (recommended)
+  required_version = "~> 1.0"
+
+  # Require the latest 2.x version of the New Relic provider
+  required_providers {
+    newrelic = {
+      source  = "newrelic/newrelic"
+    }
+  }
+}
+
+provider "newrelic" {
+  account_id = 4438260   # Your New Relic account ID
+  api_key = "NRAK-O7A6CU72Y90SJ7R5S4EMV03SIL3" # Your New Relic user key
+  region = "US"        # US or EU (defaults to US)
+}
